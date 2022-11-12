0c47d9e19a0a7743a59b481aea71e3ff2cea4e8c
Dave14 <Dave14@gmail.com>
2022-11-12 23:11:19 +0900

    Updates

--- src/Group18_Barcellano/disemvowelTrolls.java
+++
@@ -1,8 +0,0 @@
-package Group18_Barcellano;
-
-public class disemvowelTrolls {
-    public static String disemvowelTrolls(String s) {
-        s = (s.replaceAll("[aeiouAEIOU]", ""));
-        return s;
-    }
-}
