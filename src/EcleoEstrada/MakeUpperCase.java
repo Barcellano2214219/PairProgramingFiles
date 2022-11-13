9a39b2b7a6c6b763a5292b9c81d189fbaf7ba341
Dave14 <Dave14@gmail.com>
2022-11-14 02:37:41 +0900

    Updates

---
+++ src/EcleoEstrada/MakeUpperCase.java
@@ -0,0 +1,10 @@
+package EcleoEstrada;
+
+/**
+ * Write a function which converts the input string to uppercase.
+ */
+class MakeUpperCase {
+    public static String MakeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
---
+++ src/EcleoEstrada/RemoveExclamationMarks.java
@@ -0,0 +1,11 @@
+package EcleoEstrada;
+
+/**
+ * Write function RemoveExclamationMarks which removes all exclamation marks
+ * from a given string.
+ */
+public class RemoveExclamationMarks {
+    public static String removeExclamationMarks(String s) {
+        return s.replaceAll("!", "");
+    }
+}
